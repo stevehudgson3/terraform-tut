83313da7f4ab62cb4e87c67e191a4cbef9968485
stevehudgson3 <steve.allen96@hotmail.com>
2022-10-18 15:19:09 -0500

    added more options

--- prod.tf
+++ prod.tf
@@ -3,9 +3,66 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_s3_bucket_acl" "prod_tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-10299242"
   acl    = "private"
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    "Terrform" : "true"
+  }
+}
+
+resource "aws_instance" "prod_web" {
+  count = 2
+
+  ami           = "ami-06cecf34d035b98b4"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id # string interpolation doesn't need quotes "" and $ anymore
+  ]
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_eip_association" "prod_web" {
+  instance_id = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
+
+resource "aws_eip" "prod_web" {
+  tags = {
+    "Terraform" : "true"
+  }
+
+}
