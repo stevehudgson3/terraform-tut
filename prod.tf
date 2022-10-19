2e539b821df20df890c1bc4cbcdd888814d77a90
stevehudgson3 <steve.allen96@hotmail.com>
2022-10-18 19:50:06 -0500

    lb

--- prod.tf
+++ prod.tf
@@ -6,9 +6,27 @@ provider "aws" {
 resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-10299242"
   acl    = "private"
+  tags = {
+    "Terrform" : "true"
+  }
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+  tags = {
+    "Terrform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+  tags = {
+    "Terrform" : "true"
+  }
+}
+
 
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
@@ -64,5 +82,21 @@ resource "aws_eip" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
+}
 
+resource "aws_elb" "prod_web" {
+  name = "prod-web" # can't use prod_web
+  instances = aws_instance.prod_web.*.id
+  subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  tags = {
+    "Terraform" : "true"
+  }
 }
